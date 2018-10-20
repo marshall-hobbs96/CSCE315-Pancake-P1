f6443fbe5eaa22e8414315dcea143dfa3021e656
Troy <troyfulton@tamu.edu>
2018-10-20 13:00:59 -0500

    Moved tests to test branch

---
+++ test/test_graphicsEngine.cpp
@@ -0,0 +1,27 @@
+/*
+CSCE 315 503
+Project 3
+Group 17
+
+Abdul Campos
+Marshall Hobbs
+McLain Johnson
+Troy Fulton
+
+test_graphicsEngine.cpp - unit tests for testing the functionality of the Grapics Engine
+*/
+
+/************************************************
+ *  INCLUDES
+ ***********************************************/
+#include "GraphicsEngine.h"
+
+/************************************************
+ *  DEFINES
+ ***********************************************/
+
+/************************************************
+ *  TESTS
+ ***********************************************/
+
+//...
---
+++ test/test_player.cpp
@@ -0,0 +1,27 @@
+/*
+CSCE 315 503
+Project 3
+Group 17
+
+Abdul Campos
+Marshall Hobbs
+McLain Johnson
+Troy Fulton
+
+test_player.cpp - unit tests for testing the functionality of the Player
+*/
+
+/************************************************
+ *  INCLUDES
+ ***********************************************/
+#include "Player.h"
+
+/************************************************
+ *  DEFINES
+ ***********************************************/
+
+/************************************************
+ *  TESTS
+ ***********************************************/
+
+//...
