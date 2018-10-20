c6cebc17828aa4b268c3e51b92c038f4de4bbb70
Troy <troyfulton@tamu.edu>
2018-10-20 12:40:11 -0500

    Added framework code

---
+++ src/Game.cpp
@@ -0,0 +1,96 @@
+/*
+CSCE 315 503
+Project 3
+Group 17
+
+Abdul Campos
+Marshall Hobbs
+McLain Johnson
+Troy Fulton
+
+Game.cpp - Implementations for Game methods for running the game and
+        reporting the state of the game. Includes methods for reporting
+        each player's stack, signaling moves, and dealing with the high
+        scores file.
+*/
+
+#include "Game.h"
+
+/*****************************************************
+ * PRIVATE / PROTECTED METHODS
+ ****************************************************/
+
+bool Game::checkStackOrder(int* stack, int sz) {
+    // Implementation...
+    return true;
+}
+
+/*****************************************************
+ * PUBLIC METHODS
+ ****************************************************/
+
+/* For starting the game */
+
+int* Game::generateStack(int size, int* set_order = NULL) {
+    // Implementation...
+    return NULL;
+}
+
+int* Game::getHumanStack() {
+    // Implementation...
+    return NULL;
+}
+
+int* Game::getAIStack() {
+    // Implementation...
+    return NULL;
+}
+
+int Game::getStackSize() {
+    // Implementation...
+    return 0;
+}
+
+// For getting the result of AI.calculateMove()
+int Game::getAIMove() {
+    // Implementation...
+    return 0;
+}
+
+/* For the gameplay loop */
+
+void Game::moveHuman(int pancake) {
+    // Implementation...
+}
+
+void Game::moveAI(int pancake) {
+    // Implementation...
+}
+
+bool Game::checkWin() {
+    // Implementation...
+    return true;
+}
+
+// For when the game is over:
+
+int Game::computeScore() {
+    // Implementation...
+    return 0;
+}
+
+std::string Game::getHighScores() {
+    // Implementation...
+    return "";
+}
+
+/* For dealing with the high scores file */
+
+std::string Game::read() {
+    // Implementation...
+    return "";
+}
+
+void Game::write(std::string new_score) {
+    // Implementation...
+}
