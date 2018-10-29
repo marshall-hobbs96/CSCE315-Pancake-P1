1e73bc6631aa9d707ce35fb84046b5c0d5f587c4
Troy <troyfulton@tamu.edu>
2018-10-29 02:48:05 -0500

    Added minimax tree

---
+++ src/MMTree.cpp
@@ -0,0 +1,27 @@
+/*
+CSCE 315 503
+Project 3
+Group 17
+
+Abdul Campos
+Marshall Hobbs
+McLain Johnson
+Troy Fulton
+
+MMTree.cpp - Implementation for Minimax tree to evaluate the minimax tree
+*/
+
+#include "MMTree.h"
+
+MMTree::MMTree(int* stack, int d) : depth(d) {
+    // Implementation...
+}
+
+MMTree::~MMTree() {
+    // implement...
+}
+
+int MMTree::eval() {
+    //return root->eval();
+    return 0;
+}
