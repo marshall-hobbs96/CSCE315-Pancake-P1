285f3db4bb2f7fd639a2c203bbfe0415aab002de
acampos0297 <acampos0297@tamu.edu>
2018-10-29 22:08:21 -0500

    final correction to test cases

--- test/test_Game.cpp
+++ test/test_Game.cpp
@@ -33,9 +33,9 @@ if(false)
     cout<<"Case1: Test AI Sorted & User stack not sorted"<<endl;
     int difficulty = 4;
     int n = 5;
-    int[] ai = {5,4,3,2,1};
-    int[] user = {1,3,4,5,2};
-    assert(Game::computeScore(user*, ai*, difficulty)==n);
+    int ai[] = {5,4,3,2,1};
+    int user[] = {1,3,4,5,2};
+    assert(Game::computeScore(user, ai, difficulty)==n);
     cout<<"Case 1 passed"<<endl;
   
     cout<<"Case2: Test AI Sorted & User stack sorted"<<endl;
@@ -43,7 +43,7 @@ if(false)
     n = 5;
     ai = {5,4,3,2,1};
     user = {5,4,3,2,1};
-    assert(Game::computeScore(user*, ai*, difficulty)==(n*(difficulty+1)));
+    assert(Game::computeScore(user, ai, difficulty)==(n*(difficulty+1)));
     cout<<"Case 2 passed"<<endl;
            
     cout<<"Case3: Test AI not Sorted & User stack sorted"<<endl;
@@ -51,7 +51,7 @@ if(false)
     n = 5;
     ai = {5,3,4,2,1};
     user = {5,4,3,2,1};
-    assert(Game::computeScore(user*, ai*, difficulty)==(2*n*(difficulty+1)));
+    assert(Game::computeScore(user, ai, difficulty)==(2*n*(difficulty+1)));
     cout<<"Case 3 passed"<<endl;
   
     cout<<"Case4: Test AI not Sorted & User stack not sorted"<<endl;
@@ -59,6 +59,6 @@ if(false)
     n = 5;
     ai = {5,3,4,2,1};
     user = {5,4,3,1,2};
-    assert(Game::computeScore(user*, ai*, difficulty)==-1);
+    assert(Game::computeScore(user, ai, difficulty)==-1);
     cout<<"Case 4 passed"<<endl;
 }
