bc6a2d83bb9eee4be1f6f0f57e19e8130bd6f8ef
Troy <troyfulton@tamu.edu>
2018-11-02 22:56:58 -0500

    Oops

--- src/temp_main.cpp
+++ src/temp_main.cpp
@@ -29,20 +29,22 @@ int main() {
     while (keep_playing) {
         // Gets the size of the stack and the AI's difficulty:
         // diff is [stack_size, ai_difficulty]
-        int* diff = ge.getDifficultyInput(false, '3', '2');   // size defined above main
+        int* diff = ge.getDifficultyInput(false, '5', '4');   // size defined above main
 
-        string order = ge.getOrderInput();
+        int* stack = ge.getOrderInput();
+
+         = g.generateStack(diff[0], order, false);
+
+        // Instantiates a new inaccessible game object, ignoring the previous dummy
+        ge.startGame(diff[0], diff[1], "death.txt", stack);       // filename hardcoded?
+
+        std::string username = ge.getScoresInput();
 
         //***********************************Initialize two subscreens for displaying pancakes
         WINDOW* left_window = newwin(LINES / 2, COLS / 2, 0, 0);
         WINDOW* right_window = newwin(LINES / 2, COLS / 2, 0, COLS / 2);
-        ge.drawStack(stackToString(g.getHumanStack(), g.getStackSize()), g.getStackSize(), left_window);
-        ge.drawStack(stackToString(g.getHumanStack(), g.getStackSize()), g.getStackSize(), right_window);
-
-        // Instantiates a new inaccessible game object, ignoring the previous dummy
-        ge.startGame(order[0], order[1], "filename", stack);       // filename hardcoded?
-
-        std::string username = ge.getScoresInput();
+        ge.drawStack(stackToString(g.getHumanStack(), g.getStackSize()), left_window);
+        ge.drawStack(stackToString(g.getHumanStack(), g.getStackSize()), right_window);
 
         keep_playing = ge.playGame(left_window, right_window);
     }
