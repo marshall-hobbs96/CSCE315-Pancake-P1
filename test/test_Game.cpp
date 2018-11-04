c4a10845f2dc424d922e8eea3c34e97c13224c5d
Abdul Campos <acampos0297@tamu.edu>
2018-11-04 16:33:48 -0600

    Update test/test_Game.cpp

--- test/test_Game.cpp
+++ test/test_Game.cpp
@@ -17,7 +17,7 @@ test_Game.cpp - Game Unit Testing
 
 #include "includecatch2.cpp"
 #include "../include/Game.h"
-#include <iostream> 
+#include <iostream>
 #include <array>
 
 /****************************************************
@@ -28,7 +28,6 @@ test_Game.cpp - Game Unit Testing
  * TEST CASES
  ***************************************************/
 
-/*
 TEST_CASE("Case1: Test AI Sorted & User stack not sorted")
 {
 	int difficulty = 4;
@@ -37,36 +36,38 @@ TEST_CASE("Case1: Test AI Sorted & User stack not sorted")
 	int user[] = {1,3,4,5,2};
 	Game g(n, difficulty, "scores.txt", NULL);
 
-	CHECK(g.computeScore(difficulty, n, user, ai)==n)  ;
-}/*
-/*
-cout<<"Case2: Test AI Sorted & User stack sorted"<<endl;
-difficulty = 4;
-n = 5;
-int ai1[] = {5,4,3,2,1};
-int user1[] = {5,4,3,2,1};
+	CHECK(g.computeScore(difficulty, n, user, ai)==n);
+}
+TEST_CASE("Case2: Test AI Sorted & User stack sorted")
+{
+  difficulty = 4;
+  n = 5;
+  int ai1[] = {5,4,3,2,1};
+  int user1[] = {5,4,3,2,1};
+  Game g(n, difficulty, "scores.txt", NULL);
 
-assert(computeScore(difficulty,n ,user1, ai1)==(n*(difficulty+1)));
-cout<<"Case 2 passed"<<endl;
+  CHECK(computeScore(difficulty,n ,user1, ai1)==(n*(difficulty+1)));
+}
 
-cout<<"Case3: Test AI not Sorted & User stack sorted"<<endl;
-difficulty = 4;
-n = 5;
-int ai2[] = {5,3,4,2,1};
-int user2[] = {5,4,3,2,1};
+TEST_CASE("Case3: Test AI not Sorted & User stack sorted")
+{
+  difficulty = 4;
+  n = 5;
+  int ai2[] = {5,3,4,2,1};
+  int user2[] = {5,4,3,2,1};
+  Game g(n, difficulty, "scores.txt", NULL);
+  CHECK(computeScore(difficulty,n ,user2, ai2)==(2*n*(difficulty+1)));
+}
 
-assert(computeScore(difficulty,n ,user2, ai2)==(2*n*(difficulty+1)));
-cout<<"Case 3 passed"<<endl;
+TEST_CASE("Case4: Test AI not Sorted & User stack not sorted"){
+  difficulty = 4;
+  n = 5;
+  int ai3[] = {5,3,4,2,1};
+  int user3[] = {5,4,3,1,2};
+  Game g(n, difficulty, "scores.txt", NULL);
+  CHECK(computeScore(difficulty,n ,user3, ai3)==-1);
+}
 
-cout<<"Case4: Test AI not Sorted & User stack not sorted"<<endl;
-difficulty = 4;
-n = 5;
-int ai3[] = {5,3,4,2,1};
-int user3[] = {5,4,3,1,2};
-
-assert(computeScore(difficulty,n ,user3, ai3)==-1);
-cout<<"Case 4 passed"<<endl;*/
-/*
 
 TEST_CASE("Testing whether pancakeStack in int form converts to string successfully. stackToString"){
 
@@ -76,8 +77,8 @@ TEST_CASE("Testing whether pancakeStack in int form converts to string successfu
     std::string stringStack = stackToString(stack, stackSize);
     correctStack = "+-+/n|1|/n+-+/n+---+/n| 2 |/n+---+/n+-----+/n|  3  |/n+-----+/n+-------+/n|   4   |/n+-------+/n+---------+/n|    5    |/n+---------+/n+-----------+/n|     6     |/n+-----------+/n+-------------+/n|      7      |/n+-------------+/n+---------------+/n|       8       |/n+---------------+/n+-----------------+/n|        9        |/n+---------------+/n";
 
-    check(stringStack == correctStack);
+    CHECK(stringStack == correctStack);
 
     delete stack;
 
-}*/
+}
